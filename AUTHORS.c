24d7cc402d7ad956a4033d6424e869265fa1b966
elshadyusifov <e.yusifov335@gmail.com>
2025-05-04 01:30:48 +0400

    correct AUTHORS.c file

--- AUTHORS.c
+++ AUTHORS.c
@@ -9,6 +9,6 @@ C - Simple Shell
 
 * Elshad Yusifov /e.yusifov335@gmail.com - developer - Baku (Azerbaijan)
 * Babak Yusifov /babek@div.edu.az  - developer - Baku (Azerbaijan)
-* Tamerlan Huseynguliyev  
+* Tamerlan Huseynguliyev /10737@holbertonstudents.com - developer - Baku (Azerbaijan)
 
 ## Special thanks to Islam Guliyev
