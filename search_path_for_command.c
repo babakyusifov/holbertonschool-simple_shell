0580cbeff7c2705db58ed17d52b08701b4ff5bb4
tamerlanhtm <tamerlan_htm@hotmail.com>
2025-05-04 11:30:36 +0000

    search_path_for_command.c

--- search_path_for_command.c
+++ search_path_for_command.c
@@ -18,23 +18,37 @@ char *search_path_for_command(char *command, int *status)
 		(command[0] == '/' || strncmp(command, "./", 2) == 0))
 	{
 		full_path = strdup(command);
+		*status = 0;
 		return (full_path);
 	}
 
 	path = get_path();
 	if (path == NULL)
 	{
-		*status = 1;
+		*status = 127;
 		fprintf(stderr, "./hsh: 1: %s: not found\n", command);
 		return (NULL);
 	}
 
 	path_copy = strdup(path);
+	if (path_copy == NULL)
+	{
+		*status = 127;
+		return (NULL);
+	}
+
 	dir = strtok(path_copy, ":");
 
 	while (dir)
 	{
 		full_path = malloc(strlen(dir) + strlen(command) + 2);
+		if (full_path == NULL)
+		{
+			free(path_copy);
+			*status = 127;
+			return (NULL);
+		}
+
 		strcpy(full_path, dir);
 		strcat(full_path, "/");
 		strcat(full_path, command);
@@ -42,6 +56,7 @@ char *search_path_for_command(char *command, int *status)
 		if (access(full_path, X_OK) == 0)
 		{
 			free(path_copy);
+			*status = 0;
 			return (full_path);
 		}
 
@@ -49,9 +64,8 @@ char *search_path_for_command(char *command, int *status)
 		dir = strtok(NULL, ":");
 	}
 
-	fprintf(stdout, "./hsh: 1: %s: not found\n", command);
-	*status = 0;
-	free(path);
+	fprintf(stderr, "./hsh: 1: %s: not found\n", command);
+	*status = 127;
 	free(path_copy);
 	return (NULL);
 }
