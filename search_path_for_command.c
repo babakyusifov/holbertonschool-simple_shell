ecb80f887151e61da08f71c4dd9ae64069db2fb4
tamerlanhtm <tamerlan_htm@hotmail.com>
2025-05-04 11:34:06 +0000

    search_path_for_command.c

--- search_path_for_command.c
+++ search_path_for_command.c
@@ -1,8 +1,4 @@
 #include "main.h"
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
 
 char *search_path_for_command(char *command, int *status)
 {
@@ -11,63 +7,39 @@ char *search_path_for_command(char *command, int *status)
 	char *dir = NULL;
 	char *full_path = NULL;
 
-	if (command == NULL || *command == '\0')
+	if (command == NULL)
+		return (NULL);
+
+	if (strlen(command) == 0)
 		return (NULL);
 
 	if (access(command, F_OK) == 0 &&
-		(command[0] == '/' || strncmp(command, "./", 2) == 0))
+		(command[0] == '/' || command[1] == '/' || command[2] == '/'))
 	{
 		full_path = strdup(command);
-		*status = 0;
 		return (full_path);
 	}
-
 	path = get_path();
 	if (path == NULL)
 	{
-		*status = 127;
-		fprintf(stderr, "./hsh: 1: %s: not found\n", command);
+		*status = 127, fprintf(stderr, "./hsh: 1: %s: not found\n", command);
 		return (NULL);
 	}
-
 	path_copy = strdup(path);
-	if (path_copy == NULL)
-	{
-		*status = 127;
-		return (NULL);
-	}
-
 	dir = strtok(path_copy, ":");
-
 	while (dir)
 	{
 		full_path = malloc(strlen(dir) + strlen(command) + 2);
-		if (full_path == NULL)
+		strcpy(full_path, dir), strcat(full_path, "/"), strcat(full_path, command);
+		if (access(full_path, F_OK) == 0)
 		{
 			free(path_copy);
-			*status = 127;
-			return (NULL);
-		}
-
-		strcpy(full_path, dir);
-		strcat(full_path, "/");
-		strcat(full_path, command);
-
-		if (access(full_path, X_OK) == 0)
-		{
-			free(path_copy);
-			*status = 0;
 			return (full_path);
 		}
-
-		free(full_path);
-		full_path = NULL;
-		dir = strtok(NULL, ":");
+		free(full_path), dir = strtok(NULL, ":");
 	}
-
 	fprintf(stderr, "./hsh: 1: %s: not found\n", command);
 	*status = 127;
 	free(path_copy);
 	return (NULL);
 }
-
