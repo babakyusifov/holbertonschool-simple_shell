e67fac135272aeeb53a8970118249ffc68a73726
tamerlanhtm <tamerlan_htm@hotmail.com>
2025-05-04 11:46:10 +0000

    string_to_words_array.c

--- string_to_words_array.c
+++ string_to_words_array.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 char **string_to_words_array(char *line, int *status)
 {
 	char *line_copy = NULL;
@@ -9,49 +10,29 @@ char **string_to_words_array(char *line, int *status)
 	size_t i = 0;
 
 	line_copy = strdup(line);
-	if (line_copy == NULL)
-	{
-		*status = 1;
-		return (NULL);
-	}
-
 	arg = strtok(line_copy, " \n");
 	while (arg)
 	{
 		argc++;
 		arg = strtok(NULL, " \n");
 	}
+	free(line_copy);
 
 	if (argc == 0)
 	{
-		free(line_copy);
-		argv = malloc(sizeof(char *) * 1);
-		if (argv == NULL)
-		{
-			*status = 1;
-			return (NULL);
-		}
-		argv[0] = NULL;
-		return (argv);
-	}
-
-	argv = malloc(sizeof(char *) * (argc + 1));
-	if (argv == NULL)
-	{
-		free(line_copy);
-		*status = 1;
 		return (NULL);
 	}
 
+	argv = malloc(sizeof(char *) * (argc + 1));
 	arg = strtok(line, " \n");
 	for (i = 0; i < argc; i++)
 	{
-		argv[i] = arg;
+		*(argv + i) = arg;
 		arg = strtok(NULL, " \n");
 	}
 
-	argv[argc] = NULL;
-	free(line_copy);
+	argv[i] = NULL;
+
+	(void)status;
 	return (argv);
 }
-
